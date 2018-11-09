e7802e51b620052e54a61e95dfbcd592461c07a7
Daniel Weibel <danielmweibel@gmail.com>
2018-11-08 20:22:30 -0500

    Fix some typos in template

--- stack.sh
+++ stack.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-stack=$1
-cmd=$2
-
-if [[ -z "$stack" || -z "$cmd" ]]; then
-  echo "Usage: stack.sh <stack-name> up|down"
-  exit 1
-fi
-
-if [ "$cmd" = up ]; then
-  aws cloudformation deploy --template-file eks.yml --stack-name "$stack" --capabilities CAPABILITY_IAM
-else
-  aws cloudformation delete-stack --stack-name "$stack"
-fi
+aws cloudformation deploy \
+  --template-file eks.yml \
+  --stack-name eks-test \
+  --capabilities CAPABILITY_IAM \
+  --parameter-overrides KeyPairName=qs-master
