2ad5d26fbb0dc74bc2f32b31de07f761d1b38a1e
Daniel Weibel <danielmweibel@gmail.com>
2018-11-09 18:03:43 -0500

    Improve output of script; allow args to AWS CLI
    Any arguments passed to the up.sh script will be passed to the AWS CLI.
    So, it's possible to call the script with 'up.sh --region eu-west-1' to
    create a cluster in the eu-west-1 region, rather than in the default
    region.

--- up.sh
+++ up.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
-# EDIT VARIABLE VALUES:
-STACK_NAME=eks-test
-PARAM_KEY_PAIR_NAME=qs-master
-PARAM_NUM_WORKER_NODES=2
-PARAM_WORKER_NODES_INSTANCE_TYPE=t2.xlarge
+# EDIT THESE VARIABLE VALUES:
+STACK_NAME=eks-sent2vec
+KEY_PAIR_NAME=qs-eu-west-1
+NUM_WORKER_NODES=2
+WORKER_NODES_INSTANCE_TYPE=t2.xlarge
 
-echo "Deploying CloudFormation stack (may take up to 15 minutes)..."
+# Output colours
+COL='\033[1;34m'
+NOC='\033[0m'
+
+echo -e  "$COL> Deploying CloudFormation stack (may take up to 15 minutes)...$NOC"
 aws cloudformation deploy \
   --template-file eks.yml \
   --capabilities CAPABILITY_IAM \
   --stack-name "$STACK_NAME" \
   --parameter-overrides \
-      KeyPairName="$PARAM_KEY_PAIR_NAME" \
-      NumWorkerNodes="$PARAM_NUM_WORKER_NODES" \
-      WorkerNodesInstanceType="$PARAM_WORKER_NODES_INSTANCE_TYPE"
+      KeyPairName="$KEY_PAIR_NAME" \
+      NumWorkerNodes="$NUM_WORKER_NODES" \
+      WorkerNodesInstanceType="$WORKER_NODES_INSTANCE_TYPE" \
+  "$@"
 
-echo "Updating kubeconfig file..."
-aws eks update-kubeconfig --name "$STACK_NAME"
+set -e
 
-echo "Configuring worker nodes (to join the cluster)..."
+echo -e "\n$COL> Updating kubeconfig file...$NOC"
+aws eks update-kubeconfig --name "$STACK_NAME" "$@"
+
+echo -e "\n$COL> Configuring worker nodes (to join the cluster)...$NOC"
 # Get worker nodes role ARN from CloudFormation stack output
 arn=$(aws cloudformation describe-stacks \
   --stack-name "$STACK_NAME" \
   --query "Stacks[0].Outputs[?OutputKey=='WorkerNodesRoleArn'].OutputValue" \
-  --output text)
+  --output text \
+  "$@")
 # Enable worker nodes to join the cluster:
 # https://docs.aws.amazon.com/eks/latest/userguide/getting-started.html#eks-create-cluster
 cat <<EOF | kubectl apply -f -
@@ -42,5 +50,5 @@ data:
         - system:nodes
 EOF
 
-echo "Almost done! Cluster will be ready when all nodes have a 'Ready' status."
-echo "Check it with: kubectl get nodes --watch"
+echo -e "\n$COL> Almost done! Cluster will be ready when all nodes have a 'Ready' status."
+echo -e "> Check it with: kubectl get nodes --watch$NOC"
